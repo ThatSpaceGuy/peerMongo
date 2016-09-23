d74b80ca814f7c4a1e7eaa20606717e9c607d71f
Luis H. Matos <ReachLuis@Gmail.com>
2016-09-23 11:56:38 -0500

    postController front end complete

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -10,14 +10,19 @@ var allItems = [];
 
 /// == JavaScript == ///
 
-myApp.controller('mainController', ['$scope','$http',function($scope,$http){
+myApp.controller('postController', ['$scope','$http',function($scope,$http){
   console.log('NG');
 
   $scope.addItem = function(){
     console.log('in addItem', $scope.newItem);
 
     var newObject = {
-      title: $scope.newItem
+      assignment_name: $scope.assignTitle,
+      student_name: {
+        first: $scope.firstName,
+        last: $scope.lastName
+      },
+      score: $scope.assignScore
     }; // end new item
     console.log('sending:',newObject);
     // test send via http to post Route
@@ -33,3 +38,31 @@ myApp.controller('mainController', ['$scope','$http',function($scope,$http){
     console.log('allItems:', allItems);
   };
 }]);
+
+myApp.controller('showController', ['$scope','$http',function($scope,$http){
+  console.log('NG');
+
+  $scope.addItem = function(){
+    console.log('in addItem', $scope.newItem);
+
+    var newObject = {
+      assignment_name: $scope.assignTitle,
+      student_name: {
+        first: $scope.firstName,
+        last: $scope.lastName
+      },
+      score: $scope.assignScore
+    }; // end new item
+    console.log('sending:',newObject);
+    // test send via http to post Route
+    $http({
+      method: 'POST',
+      url: '/testPost',
+      data: newObject
+    }).then(function ( response ){
+      console.log('back from server with:', response);
+    });
+
+    allItems.push(newObject);
+    console.log('allItems:', allItems);
+  };
